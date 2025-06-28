868c0798bfe4a1de3b555a6dfe83436b9951b30e
kavanaraghunathcodingal <Kavanaraghunath.codingal@gmail.com>
2025-06-28 19:44:02 +0530

    Add files via upload

---
+++ LOOP TIME.PY
@@ -0,0 +1,17 @@
+def myfunction(n):
+  # Loop 1  
+  for i in range(0,n+1):
+      print("First Loop")
+
+  # Loop 2
+  j=1
+  while(j<=n+1):
+      print("Second Loop ",j)
+      j=j*2
+
+  # Loop 3
+  for i in range(0,100):
+      print("Third loop")
+
+print("Function above will take time :")
+print(" O(N) +  O(log N) + O(1)")
